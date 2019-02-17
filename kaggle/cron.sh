5892476a6ec1d9128df0f39d03279e222b4a47ab
doodeck <8515450+doodeck@users.noreply.github.com>
2019-02-17 10:12:06 +0000

    begin/end time logged

--- kaggle/cron.sh
+++ kaggle/cron.sh
@@ -3,9 +3,10 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 logfile="`basename $0`.log"
 . ../../bin/activate
-echo "### `date`" >> $logfile 2>&1
+echo "### Start >>> `date`" >> $logfile 2>&1
 python nextbike_dataset_scraper.py >> $logfile 2>&1
 
 if (( `date +%H` == 0 && `date +%M` < 20)); then
   kaggle datasets version --message "cron update `date`" -p ./DB/ >> $logfile 2>&1
 fi
+echo "### End <<< `date`" >> $logfile 2>&1
