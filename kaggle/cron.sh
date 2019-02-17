732e847d506261a01b710468c83e3a580fc0379a
doodeck <8515450+doodeck@users.noreply.github.com>
2019-02-17 09:14:04 +0000

    stderr diverted too log

--- kaggle/cron.sh
+++ kaggle/cron.sh
@@ -3,9 +3,9 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 logfile="`basename $0`.log"
 . ../../bin/activate
-echo "### `date`" >> $logfile
-python nextbike_dataset_scraper.py >> $logfile
+echo "### `date`" >> $logfile 2>&1
+python nextbike_dataset_scraper.py >> $logfile 2>&1
 
-if (( `date +%H` == 0 && `date +%M` < 10)); then
-  kaggle datasets version --message "cron update `date`" -p ./DB/ >> $logfile
+if (( `date +%H` == 0 && `date +%M` < 20)); then
+  kaggle datasets version --message "cron update `date`" -p ./DB/ >> $logfile 2>&1
 fi
